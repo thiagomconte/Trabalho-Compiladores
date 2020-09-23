abb914ee05f9970f90b7c4bf5b8a234ffb90337f
thiagomconte <thiagomconte@hotmail.com>
2020-09-23 14:06:16 -0300

    Quase pronto
    Falta arrumar System.out.println

--- lexico.py
+++ lexico.py
@@ -4,41 +4,70 @@ from sly import Lexer
 class CalcLexer(Lexer):
     # Set of token names. This is always required 
 
-    tokens = { ID, NUMBER, PLUS, MINUS, TIMES,
-               DIVIDE, ASSIGN, AND, LT, LE, GT, GE,
-               IF, WHILE, ELSE, RETURN, CLASS, PRINT, INT, METH }
+    tokens = { ID, NUM, PLUS, MINUS, TIMES,
+               DIVIDE, EQUAL, AND, LESS,
+               KEYWORD, SCOLON, COLON, LPAREN, LBRACK, RBRACK,
+               RPAREN, LBRACE, RBRACE, DOT, NOT, WRITE }
     
-    
-    literals = {'(', ')', '{', '}', ';', '[', ']', '.'}
     
     # String containing ignored characters between tokens
     ignore = ' \t'
+    ignore_newline = r'\n+'
     
     # Regular expression rules for tokens
-    PRINT = r'System.out.println'
+    ID['System.out.println'] = KEYWORD
     ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    ID['if'] = IF
-    ID['while'] = WHILE
-    ID['else'] = ELSE
-    ID['return'] = RETURN
-    ID['class'] = CLASS
-    ID['int'] = INT
-    ID['public'] = METH
-    ID['private'] = METH
-    NUMBER  = r'\d+'
+    ID['if'] = KEYWORD
+    ID['while'] = KEYWORD
+    ID['else'] = KEYWORD
+    ID['return'] = KEYWORD
+    ID['class'] = KEYWORD
+    ID['int'] = KEYWORD
+    ID['public'] = KEYWORD
+    ID['private'] = KEYWORD
+    ID['static'] = KEYWORD
+    ID['void'] = KEYWORD
+    ID['main'] = KEYWORD
+    ID['extends'] = KEYWORD
+    ID['boolean'] = KEYWORD
+    ID['true'] = KEYWORD
+    ID['false'] = KEYWORD
+    ID['this'] = KEYWORD
+    ID['new'] = KEYWORD
+    ID['length'] = KEYWORD
+    SCOLON = r'\;'
+    COLON = r'\,'
+    LPAREN = r'\('
+    RPAREN = r'\)'
+    LBRACE = r'\{'
+    RBRACE = r'\}'
+    LBRACK = r'\['
+    RBRACK = r'\]'
+    DOT = r'\.'
+    NOT = r'!'
+    NUM  = r'\d+'
     PLUS    = r'\+'
     MINUS   = r'-'
     TIMES   = r'\*'
     DIVIDE  = r'/'
-    ASSIGN  = r'='
+    EQUAL  = r'='
     AND = r'&&'
-    LE = r'<='
-    LT = r'<'
-    GE = r'>='
-    GT = r'>'
+    LESS = r'<'
+    
+    #AÇÃO PARA DETECTAR QUEBRA DE LINHA E INCREMENTÁ-LA
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += len(t.value)
+    
+    def error(self, t):
+        print("Linha %d - Caractere ilegal : %s" % (t.lineno, t.value[0]))
+        self.index += 1
 
 if __name__ == '__main__':
-    data = input('lex > ') 
+    file = input('Nome do arquivo > ')
+    myfile = open(file, 'r')
+    data = myfile.read()
     lexer = CalcLexer()
     for tok in lexer.tokenize(data):
-        print('type=%r, value=%r lineno=%r index=%r' % (tok.type, tok.value, tok.lineno, tok.index))
+        print('[%r , %r , %r]' % (tok.lineno, tok.type, tok.value))
+    myfile.close()    
